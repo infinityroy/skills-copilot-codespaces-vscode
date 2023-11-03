1b12f24c965d90ba9e90e7937a93ccf72c333c3b
Pandita <146020318+pandita8743@users.noreply.github.com>
2023-11-03 00:08:34 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,5 @@
+function calculateNumbers(var1, var2)          // Function declaration
+{
+    var total = var1 + var2;                      // Function definition
+    return total;                                 // Function returns the total
+}
